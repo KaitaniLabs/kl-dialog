455bf0531a72567281467a0a8f40fb5653786478
Cal West <calwest@Cals-iMac.local>
2018-01-10 19:25:31 +0000

    adds title option to template

--- client/mechanics.js
+++ client/mechanics.js
@@ -1,4 +1,12 @@
 
 Template.KL_Dialog.events({
-	
+	'click [data-action="dialogDismiss"]': function(e,tmpl){
+		Dialog.clear(true);
+	},
+});
+
+Template.KL_Dialog.helpers({
+	title : () => {
+		return ((Object.prototype.toString.call(Dialog.options.title) === "[object Function]") ? Dialog.options.title() : Dialog.options.title);
+	},
 });
